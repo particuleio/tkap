91303f6fb6b7c10e0b13df4bdbadcf042c55c82b
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-09-17 23:20:38 +0200

    chore: fix backend
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- terraform/live/demo/fr-par/kapsule/backend.tf
+++ terraform/live/demo/fr-par/kapsule/backend.tf
@@ -1,10 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket                      = "tkap-terraform-remote-state"
-    key                         = "demo"
-    region                      = "fr-par"
-    endpoint                    = "https://s3.fr-par.scw.cloud"
-    skip_region_validation      = true
-    skip_credentials_validation = true
+  backend "remote" {
+    organization = "clusterfrak-dynamics"
+
+    workspaces {
+      name = "tkap"
+    }
   }
 }
