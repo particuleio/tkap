202100d4fe13a7cb6199169badb9206f44ebd213
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-09-17 22:53:39 +0200

    feat: initial commit
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

---
+++ terraform/live/demo/fr-par/kapsule/main.tf
@@ -0,0 +1,14 @@
+module "kapsule" {
+  source              = "github.com/clusterfrak-dynamics/terraform-scaleway-kapsule?ref=v1.0.0"
+  cluster_name        = "tkap"
+  cluster_description = "tkap"
+
+  node_pools = {
+    tkap = {
+      size        = 2
+      max_size    = 3
+      min_size    = 1
+      autoscaling = true
+    }
+  }
+}
