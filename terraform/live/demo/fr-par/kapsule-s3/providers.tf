5f4ca8644642f741b5f060ffed7a0a9da71ec69f
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-11-08 01:44:41 +0100

    fix: remove kubectl for s3 env
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- terraform/live/demo/fr-par/kapsule-s3/providers.tf
+++ terraform/live/demo/fr-par/kapsule-s3/providers.tf
@@ -13,10 +13,3 @@ provider "helm" {
     load_config_file       = false
   }
 }
-
-provider "kubectl" {
-  host                   = module.kapsule.kubeconfig[0]["host"]
-  cluster_ca_certificate = base64decode(module.kapsule.kubeconfig[0]["cluster_ca_certificate"])
-  token                  = module.kapsule.kubeconfig[0]["token"]
-  load_config_file       = false
-}
