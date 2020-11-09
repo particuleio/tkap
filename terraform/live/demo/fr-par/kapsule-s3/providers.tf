c7f74a01a296cce405ec709d22629dda959aa923
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-11-09 01:12:21 +0100

    fix: add kubectl provider
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- terraform/live/demo/fr-par/kapsule-s3/providers.tf
+++ terraform/live/demo/fr-par/kapsule-s3/providers.tf
@@ -5,6 +5,13 @@ provider "kubernetes" {
   load_config_file       = false
 }
 
+provider "kubectl" {
+  host                   = module.kapsule.kubeconfig[0]["host"]
+  cluster_ca_certificate = base64decode(module.kapsule.kubeconfig[0]["cluster_ca_certificate"])
+  token                  = module.kapsule.kubeconfig[0]["token"]
+  load_config_file       = false
+}
+
 provider "helm" {
   kubernetes {
     host                   = module.kapsule.kubeconfig[0]["host"]
